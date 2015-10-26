e125b2afae51934f8206fb23a5bc38e1fc7021cb
jihaeko <minimind@naver.com>
2015-10-26 15:24:43 +0900

    Lec1026
    중간고사 & 정규표현식2

---
+++ PLec13/PLec13/PLec13.py
@@ -0,0 +1,125 @@
+﻿import re
+import os
+import sys
+import glob
+import urllib.request
+
+
+#str = '''Window
+#Unix
+#Linux
+#Solaris'''
+
+#p = re.compile('^.+')
+#print(p.findall(str))
+
+#p = re.compile('^.+', re.M)     #멀티라인
+#print(p.findall(str))
+
+#p = re.compile('^.+', re.M)
+#print(p.search(str))
+
+
+
+
+
+#m = re.match(r"(?P<first>\w+) (?P<last>\w+)", "Jihae Ko")
+#print(m.group('first', 'last'))
+#print(m.groups())
+#print(m.groupdict())
+
+#print()
+
+#m = re.match(r"(\d+)\.?(\d+)?", "24")
+#print(m.groups())
+#print(m.groups(0))
+
+
+
+
+
+
+
+##p = re.compile(".+:")
+#p = re.compile(".+(?=:)")
+#m = p.search("http://google.com")
+#print(m.group())
+
+
+
+
+
+#p = re.compile('.*[.](?!bat$|exe$).*$')
+#os.chdir("C:\\")
+#print(os.getcwd())
+#for i in glob.iglob('*'):
+#    if p.match(i):
+#        print(i)
+
+
+
+
+
+#p = re.compile("(?<=abc)def")
+#m = p.search("abcdef")
+#print(m.group())
+
+#m = re.search('(?<=-)\w+', 'spam-egg')
+#print(m.group())
+
+
+
+
+
+
+#email = "jihae@navremove_thiser.com"
+#m = re.search("remove_this", email)
+#print(m.start())
+#print(m.end())
+#result = email[:m.start()] + email[m.end():]
+#print(result)
+
+
+
+
+
+
+#def displaymatch(match):
+#    if match is None:
+#        return None
+#    return '<Match: %s, groups=%s>' % (match.group(), match.groups())
+
+#valid = re.compile(r"^[a2-9tjqk]{5}$")
+#print(displaymatch(valid.match("akt5q")))
+#print(displaymatch(valid.match("akt5e")))
+#print(displaymatch(valid.match("akt")))
+#print(displaymatch(valid.match("727ak")))
+
+
+
+
+
+#text = """Ross: McFluff: 834.345.1254: 155 Elm Street
+#Ronald: Heathmore: 892.345.3428 436: Finley Avenue
+#Frank: Burger: 925.541.7625 662: South Dogwood Way
+#Heather: Albrecht: 548.326.4584 919: Park Place"""
+
+#entries = re.split("\n", text)
+#result = [re.split(":?", entry, 4) for entry in entries]
+#print(result)
+
+
+
+
+
+
+with urllib.request.urlopen('http://www.naver.com/') as f:
+    #print(f.read())
+    #print(f.read(100)) #300byte
+    #print(f.read(1000).decode("utf-8")) #encoding
+    
+    txt = str(f.read())
+    title_start = re.search("<title>",txt)
+    title_end = re.search("</title_>",txt)
+    title = txt[title_start.end():title_end.start()]
+    print(title)
---
+++ 고지혜/고지혜/고지혜.py
@@ -0,0 +1,92 @@
+﻿import os
+import sys
+import time
+
+class MidTest:
+    def __init__(self, num, name):
+        self.num = num
+        self.name = name
+
+    def question1(self):
+        print("1) 학번 :" + self.num + ", 이름 : " + self.name)
+
+    def question2(self, inputData, num):
+        result = []
+        sortedData = sorted(data.items(), key=lambda x: x[1])
+
+        print("2) ", end = "")
+        for steps in range(num):
+            result.append(sortedData[(steps+1)*(-1)])
+        print(result)
+
+    def question3(self, inputFileName):
+        fileName = "score\\"+inputFileName
+
+        if os.path.exists(fileName):
+            os.remove(fileName)
+
+        with open(fileName, "w") as wholeFile:
+            for(path, dir, files) in os.walk('.'):
+                if path == '.\\score':
+                    for name in files:
+                        subname = "score\\"+name
+                        myfile = open(subname, "r")
+                        while True:
+                            content = myfile.readline()
+                            if not content: 
+                                wholeFile.write("\n")
+                                break
+                            wholeFile.write(content)
+                        myfile.close()
+
+            print("3) "+inputFileName+"생성이 완료되었습니다.")
+
+    def question4(self, inputFileName):
+        fileName = "score\\"+inputFileName
+        print("4) ", end = "")
+
+        try:
+            with open(fileName, "r") as myFile:
+                tempsum = 0
+                tempname = ""
+                for content in myFile:
+                    try:
+                        (name, kor, eng, math) = content.strip().split(',', 3)
+                    except:
+                        pass
+                    sum = int(kor) + int(eng) + int(math)
+                    if sum > tempsum:
+                        tempsum = sum
+                        tempname = name
+                top = (tempname, tempsum)
+                print(top)
+                    
+                    
+        except FileNotFoundError:
+            error = sys.exc_info()[0:2]
+            print(error)
+   
+    @staticmethod
+    def question5():
+        print("5) Current Time : ", end="")
+        print(time.strftime("%Y.%m.%d. %I:%M:%S", time.localtime()))
+
+
+
+
+#1)
+jihae = MidTest("201311193", "고지혜")
+jihae.question1()
+
+#2)
+data = {"명량":4.5, "베테랑":4.0, "암살":4.6, "마션":4.3}
+jihae.question2(data, 2)
+
+#3)
+jihae.question3("question3.txt")
+
+#4)
+jihae.question4("question3.txt")
+
+#5)
+MidTest.question5()
