5921dbe2993f84eadb7570365fe1e3bbc9079705
jihaeko <minimind@naver.com>
2015-11-02 13:56:35 +0900

    Lec1102
    XML, Json 파싱 & 퀴즈

---
+++ PLec15/PLec15/PLec15.py
@@ -0,0 +1,97 @@
+﻿from bs4 import BeautifulSoup
+
+#XML
+
+#f = open('test.xml')
+#xml = f.read()
+
+#soup = BeautifulSoup(xml)
+#for node in soup.findAll('node'):
+#    print("Node: "+node.string)
+#    print("Attr1: "+node['attr1'])
+
+
+
+
+
+#f = open('song.xml', encoding='utf-8')
+#xml = f.read()
+
+#soup = BeautifulSoup(xml)
+#for nodes in soup.test('song'):
+#    for node in nodes:
+#        print(node.string)
+
+
+
+
+
+#f = open('alcohol.xml', encoding='utf-8')
+#xml = f.read()
+
+#soup = BeautifulSoup(xml, 'lxml')
+#for nodes in soup.alcohol('cate1'):
+#    if nodes['tt'] == "안주":
+#        print('Cate1: ' + nodes['tt'])
+#        for node in nodes('cate2'):
+#            print('\tCate2: '+node['tt'])
+#            for item in node('item'):
+#                print("\t\t" + item.string)
+
+
+
+
+#Json
+import json
+#data = {1:'a', 2:'b'}
+#data2 = json.dumps(data)
+#data3 = json.loads(data2)
+
+#print(type(data2))
+#print(data2)
+#print(type(data3))
+#print(data3)
+
+
+
+
+#data = {1:"우리", 2:"나라"}
+#data2 = json.dumps(data, ensure_ascii=False)
+#print(data2)
+#print(type(data2))
+
+
+
+
+
+
+s = """
+{
+"name" : "cybaek",
+"detail" : {"last":"baek"},
+"emails": ["cybaek@xxx.com", "cybaek@yyy.com"]
+}
+"""
+
+#data = json.loads(s)
+
+#print(data['name'])
+#print(data['detail'])
+#print(data['detail']['last'])
+
+
+
+
+class JsonObject:
+    def __init__(self, d):
+        self.__dict__ = d
+
+data = json.loads(s, object_hook = JsonObject)
+print(data.name)
+print(data.detail)
+print(data.detail.last)
+for email in data.emails:
+    print(email)
+
+
+
---
+++ PQuiz/PQuiz/PQuiz.py
@@ -0,0 +1,24 @@
+﻿import re
+
+def checkPW(input):
+    p = re.compile(".{8,13}")
+    if p.match(input):
+        p2 = re.compile("\w+[!@#$%^&*]+")
+        p3 = re.compile("[!@#$%^&*]+\w+")
+        if p2.match(input) or p3.match(input):
+            return True
+        else:
+            print("비밀번호는 알파벳, 숫자, 특수문자가 모두 포함되어야함")
+            return False
+
+    else:
+        print("비밀번호는 8자리 이상 13자리 이하여야함")
+        return False
+
+
+result = checkPW("12345678901")
+print(result)
+
+
+
+
