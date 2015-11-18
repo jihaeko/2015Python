1ea8df765bd337523828a889d7862c53170fda9f
jihaeko <minimind@naver.com>
2015-11-18 13:46:46 +0900

    Lec1118
    플라스크 render html

--- PLec18/PLec18/PLec18.py
+++ PLec18/PLec18/PLec18.py
@@ -1,12 +1,28 @@
-﻿from flask import Flask, url_for, redirect
+﻿from flask import Flask, url_for, redirect, render_template
 
 #the name of the application package
 app = Flask(__name__)
 
-@app.route('/hello')     #경로 지정해주는것. 홈페이지의 메인에 해당하는거 접속했을때! 
-def hello_world():
-    return redirect(url_for('login'))
-   
+@app.route('/hello/')
+@app.route('/hello/<title>/<name>')     #경로 지정해주는것. 홈페이지의 메인에 해당하는거 접속했을때! 
+def hello_world(title = None, name=None):
+    data1 = [dict(href="http://www.naver.com", caption="네이버"), dict(href="http://www.google.com", caption="구글")]
+    data2 = {
+        'title' : title,
+        'name' : name
+        }
+    #return render_template('hello.html', items=data, name=name)
+    return render_template('hello.html', items=data1, **data2)
+
+@app.route('/')   
+def main(title = None, name=None):
+    data1 = [dict(href="http://www.naver.com", caption="네이버"), dict(href="http://www.google.com", caption="구글")]
+    data2 = {
+        'title' : title,
+        'name' : name
+        }
+    return render_template('main.html', items=data1, **data2)
+    
 
 @app.route('/profile/<username>')
 def get_profile(username):
